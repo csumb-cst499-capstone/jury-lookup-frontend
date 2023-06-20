6cf4e5b7a0d3a2d20856fea6d83459303341c043
Layla Gallez <layla.gallez@gmail.com>
2023-06-19 17:15:41 -0700

    added postpone componet

--- components/postpone.jsx
+++ components/postpone.jsx
@@ -1,59 +1,54 @@
 import React, { useState, useEffect } from "react";
 import Calendar from 'react-calendar';
+import Days from "react-calendar/dist/cjs/MonthView/Days";
+const CONSTANT = require('../constants/JUROR_CONSTANTS')
 
 export function Postpone({ data = [] }) {
   const [value, onChange] = useState(new Date());
-  const [requestStatus, setRequestStatus] = useState(null); // Track the request status
+  const [requestStatus, setRequestStatus] = useState(null);
 
   const handleDateChange = async (date) => {
-    const currentDate = new Date();
-    const summonDate = new Date(apiData.summonDate);
-  
-    // Check if selected date is before the summon date
-    if (date < summonDate) {
-      console.log("Selected date is before the summon date");
-      // You can display an error message or take appropriate action here
-      return;
-    }
-  
-    // Calculate the maximum allowed date (6 weeks after the summon date)
-    const maxDate = new Date(summonDate.getTime() + 6 * 7 * 24 * 60 * 60 * 1000);
-  
-    // Check if selected date is more than 6 weeks past the summon date
-    if (date > maxDate) {
-      console.log("Selected date is more than 6 weeks past the summon date");
-      // You can display an error message or take appropriate action here
-      return;
-    }
-  
-    // Check if selected date is not a Monday (day number 1)
-    if (date.getDay() !== 1) {
-      console.log("Selected date is not a Monday");
-      // You can display an error message or take appropriate action here
-      return;
-    }
-  
     onChange(date);
-  
+
+    const currentDate = new Date();
+    // const maxDate = will get from the user state once implemented
     const formattedDate = date.toISOString().split("T")[0]; // Format date as "YYYY-MM-DD"
-    const url = `http://localhost:3000/api/postponeSummon/322976632/489062/${formattedDate}`;
-  
-    try {
-      const res = await fetch(url, { method: "PUT" });
-      if (res.ok) {
-        // Handle success
-        setRequestStatus("success");
-        console.log("PUT request successful");
-      } else {
-        // Handle failure
+    const url = `http://localhost:3000/api/postponeSummon/687056417/164523/${formattedDate}`;
+    const sixtyDaysFromNow = currentDate.getDate() + 60; //will change to summonDate
+
+    if (formattedDate <= currentDate.toISOString().split("T")[0]) {
+        alert("Please select a date in the future.");
         setRequestStatus("failure");
-        console.log("PUT request failed");
-      }
-    } catch (error) {
-      console.log("error: ", error);
-      setRequestStatus("failure");
+        return;
     }
-  };  
+    if (formattedDate >= sixtyDaysFromNow) { 
+        alert("Please select a date within 6 weeks of the original summon date.");
+        setRequestStatus("failure");
+        return;
+    }
+    if (date.getDay() !== 1) {
+        alert("Selected date is not a Monday");
+        setRequestStatus("failure");
+        return;
+    }
+    else {
+        try {
+            const res = await fetch(url, { method: "PUT" });
+            if (res.ok) {
+              // Handle success
+              setRequestStatus("success");
+              console.log("PUT request successful");
+            } else {
+              // Handle error
+              setRequestStatus("failure");
+              console.log("PUT request failed");
+            }
+          } catch (error) {
+            setRequestStatus("failure");
+            console.log("error: ", error);
+          }
+    }
+  };
 
   return (
 
