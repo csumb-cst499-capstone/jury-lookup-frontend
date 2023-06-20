3c5de2ed587bf7247f8b23960628d62d6fa3f521
Layla Gallez <layla.gallez@gmail.com>
2023-06-19 17:24:10 -0700

    Update page.js
    Removed import used for testing

--- app/page.js
+++ app/page.js
@@ -1,7 +1,6 @@
 'use client'
 import { Container, Grid } from '@nextui-org/react'
-// import { Greeting } from '../components/greeting'
-import { Postpone } from '../components/postpone'
+import { Greeting } from '../components/greeting'
 
 export default function Page() {
   return (
