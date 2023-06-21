c25f3fca58d4c763a6ea3ff2b563e3defe84d5ed
Yavik Kapadia <yavikkapadia@gmail.com>
2023-06-20 17:18:17 -0700

    Added Login component to page.js and dropped greeting

--- app/page.js
+++ app/page.js
@@ -1,13 +1,14 @@
 'use client'
 import { Container, Grid } from '@nextui-org/react'
 import { Greeting } from '../components/greeting'
+import Login from '@/components/login'
 
 export default function Page() {
   return (
     
     <Container fluid >
       <Grid.Container gap={2} justify="center">
-        <Greeting />
+        <Login />
       </Grid.Container>
     </Container>
   
