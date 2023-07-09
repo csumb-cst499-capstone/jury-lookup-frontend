4fd34a081a178571c74c839addd413ef3859e306
Layla Gallez <layla.gallez@gmail.com>
2023-07-08 17:16:30 -0700

    added login spec

---
+++ cypress/e2e/loginSpec.cy.js
@@ -0,0 +1,37 @@
+const { Modal } = require("@nextui-org/react");
+const { default: Calendar } = require("react-calendar");
+
+describe("Login Component", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3001"); 
+  });
+
+  it("should successfully log in with valid credentials", () => {
+    cy.intercept("POST", "http://localhost:3000/api/login", {
+      statusCode: 200,
+      body: { token: "your-token-value" },
+    }).as("loginRequest");
+
+    cy.get('input[name="badgeNumber"]').type("123");
+    cy.get('input[name="pinCode"]').type("123");
+    cy.contains("Sign In").click();
+
+    cy.wait("@loginRequest").then(() => {
+      cy.get('button:contains("Success!")').should("be.visible");
+    });
+  });
+
+  it("should display error message with invalid credentials", () => {
+    cy.intercept("POST", "http://localhost:3000/api/login", {
+      statusCode: 401,
+    }).as("loginRequest");
+
+    cy.get('input[name="badgeNumber"]').type("123");
+    cy.get('input[name="pinCode"]').type("123");
+    cy.contains("Sign In").click();
+
+    cy.wait("@loginRequest").then(() => {
+      cy.get('button:contains("Invalid Credentials")').should("be.visible");
+    });
+  });
+});
