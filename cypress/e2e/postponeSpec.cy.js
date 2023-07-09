fa8da4404766a0ea614bece95435244664054b5d
Layla Gallez <layla.gallez@gmail.com>
2023-07-09 00:26:17 -0700

    made test faster

--- cypress/e2e/postponeSpec.cy.js
+++ cypress/e2e/postponeSpec.cy.js
@@ -13,39 +13,43 @@ describe('Sucessful Postpone Component', () => {
   });
 
   it('should display "Postpone date must be a Monday" if the selected date is not a Monday', () => {
-    cy.wait(10000);
-    cy.get('abbr[aria-label="July 11, 2023"]').click();
+    cy.get('abbr[aria-label="July 11, 2023"]'
+      , { timeout: 10000 }).click();
     cy.get('button:contains("Confirm")').click();
-    cy.contains('date is not a Monday', { timeout: 10000 }).should("be.visible");
+    cy.contains('date is not a Monday'
+      , { timeout: 10000 }).should("be.visible");
   });
 
   it('should display "Postpone date must be after service date" if the selected date is before the service date', () => {
-    cy.wait(10000);
-    cy.get('button[class="react-calendar__navigation__arrow react-calendar__navigation__prev-button"]').click();
+    cy.get('button[class="react-calendar__navigation__arrow react-calendar__navigation__prev-button"]'
+      , { timeout: 10000 }).click();
     cy.get('abbr[aria-label="June 19, 2023"]').click();
     cy.get('button:contains("Confirm")').click();
-    cy.contains('Please select a date later than your original summons date.', { timeout: 10000 }).should("be.visible");
+    cy.contains('Please select a date later than your original summons date.'
+      , { timeout: 10000 }).should("be.visible");
   });
 
   it('should display "Postpone date must be within 6 weeks of service date" if the selected date is more than 6 weeks after the service date', () => {
-    cy.wait(10000);
-    cy.get('button[class="react-calendar__navigation__arrow react-calendar__navigation__next-button"]').click();
+    cy.get('button[class="react-calendar__navigation__arrow react-calendar__navigation__next-button"]'
+      , { timeout: 10000 }).click();
     cy.get('button[class="react-calendar__navigation__arrow react-calendar__navigation__next-button"]').click();
     cy.get('abbr[aria-label="August 28, 2023"]').click();
     cy.get('button:contains("Confirm")').click();
-    cy.contains('Please select a date within 6 weeks of your original summons date.', { timeout: 10000 }).should("be.visible");
+    cy.contains('Please select a date within 6 weeks of your original summons date.'
+      , { timeout: 10000 }).should("be.visible");
   });
 
   it('should display "You have successfully postponed your service date" if the selected date is valid', () => {  
-    cy.wait(10000);
-    cy.get('button[class="react-calendar__navigation__arrow react-calendar__navigation__next-button"]').click();
+    cy.get('button[class="react-calendar__navigation__arrow react-calendar__navigation__next-button"]'
+      , { timeout: 10000 }).click();
     cy.get('abbr[aria-label="August 7, 2023"]').click();
     cy.get('button:contains("Confirm")').click();
     cy.contains('You are no longer able to postpone this summon').should("be.visible");
   });
 
   it('should display "You are no longer able to postpone this summon" if CanPostpone is false', () => {
-    cy.contains('You are no longer able to postpone this summon', { timeout: 10000 }).should("be.visible");
+    cy.contains('You are no longer able to postpone this summon'
+      , { timeout: 10000 }).should("be.visible");
     resetPostponeableUserLogin();
   });
 });
--- cypress/e2e/specLogin.js
+++ cypress/e2e/specLogin.js
@@ -1,16 +1,16 @@
-const postponeableUser = { badgeNumber: "1781995622", pinCode: "877715" };
+const testUser = { badgeNumber: "1781995622", pinCode: "877715" };
 
 export function postponeableUserLogin() {
   cy.visit("http://localhost:3001");
-  cy.get('input[name="badgeNumber"]').type(postponeableUser.badgeNumber);
-  cy.get('input[name="pinCode"]').type(postponeableUser.pinCode);
+  cy.get('input[name="badgeNumber"]').type(testUser.badgeNumber);
+  cy.get('input[name="pinCode"]').type(testUser.pinCode);
   cy.contains("Sign In").click();
 };
 
 export function resetPostponeableUserLogin() {
   cy.request('POST', 'http://localhost:3000/api/login/', {
-    BadgeNumber: postponeableUser.badgeNumber,
-    PinCode: postponeableUser.pinCode
+    BadgeNumber: testUser.badgeNumber,
+    PinCode: testUser.pinCode
   }).then((response) => {
     const token = response.body.token;
     Cypress.env('token', token);
@@ -19,7 +19,7 @@ export function resetPostponeableUserLogin() {
       method: 'POST',
       url: 'http://localhost:3000/api/resetSummonsDate',
       body: {
-        badgeNumber: postponeableUser.badgeNumber
+        badgeNumber: testUser.badgeNumber
       },
       headers: {
         Authorization: token
