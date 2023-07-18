8c1ad7e59f818ecc1f6b6e9614957e4e43318ba7
Layla Gallez <layla.gallez@gmail.com>
2023-07-17 18:54:14 -0700

    added spec coverage with summon details

--- cypress/e2e/postponeSpec.cy.js
+++ cypress/e2e/postponeSpec.cy.js
@@ -14,12 +14,13 @@ describe("Sucessful Postpone Component", () => {
       , { timeout: 10000 }).click();
     cy.get('button:contains("Monterey")').click();
     cy.get('li:contains("King City")').click();
-    cy.get('abbr[aria-label="January 14, 2069"]'
+    cy.get('abbr[aria-label="January 11, 2100"]'
       , { timeout: 10000 }).click();
     cy.get('button:contains("Confirm")').click();
     cy.contains("You are no longer able to postpone this summon").should(
       "be.visible"
     );
+    cy.contains("January 11, 2100").should("be.visible");
   });
 
   it('should sucessfully change the location without changing date', () => {
@@ -29,16 +30,19 @@ describe("Sucessful Postpone Component", () => {
     cy.get('li:contains("King City")').click();
     cy.get('button:contains("Confirm")').click();
     cy.contains('You are no longer able to postpone this summon').should("be.visible");
+    cy.contains('King City').should("be.visible");
+    cy.contains('January 4, 2100').should("be.visible");
   });
 
   it('should sucessfully update the date without changing location', () => {  
     resetPostponeableUserLogin();
     cy.get('button:contains("Edit Summons")'
       , { timeout: 10000 }).click();
-    cy.get('abbr[aria-label="January 14, 2069"]'
+    cy.get('abbr[aria-label="January 11, 2100"]'
       , { timeout: 10000 }).click();
     cy.get('button:contains("Confirm")').click();
     cy.contains('You are no longer able to postpone this summon').should("be.visible");
+    cy.contains('January 11, 2100').should("be.visible");
   });
 });
 
