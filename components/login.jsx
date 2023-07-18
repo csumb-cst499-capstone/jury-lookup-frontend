ece523d831dbbd0f49da4e8d34e4a44b31217adb
AlonsoVega <alvega@csumb.edu>
2023-07-17 22:06:46 -0700

    added modal file

--- components/login.jsx
+++ components/login.jsx
@@ -1,6 +1,7 @@
 import React, { Suspense, useState } from "react";
 import { SummonDetails } from "./summon_details";
 import { Button, Input } from "@nextui-org/react";
+import Modal from "./Modal";
 import "../styles/animations.css"; // Import the CSS file containing animations
 
 function Login() {
@@ -120,30 +121,7 @@ function Login() {
           </div>
         </Suspense>
       )}
-      {showModal && (
-        <div className="fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center bg-gray-900 bg-opacity-50">
-          <div className="bg-white rounded-lg p-8">
-            <h3 className="text-2xl font-bold mb-4">Instructions for Sign In</h3>
-            <p>
-              Cannot Sign In? Follow these steps:
-              <br />
-              1. Enter your badge number in the "Badge Number" field.
-              <br />
-              2. Enter your pin code in the "Pin Code" field.
-              <br />
-              3. Click on the "Sign In" button.
-              <br />
-              If you still have trouble signing in, please call our office for assistance.
-            </p>
-            <Button
-              className="mt-4 text-sm px-3 py-1 rounded-full bg-purple-500 text-white hover:bg-purple-300"
-              onClick={closeModal}
-            >
-              Close
-            </Button>
-          </div>
-        </div>
-      )}
+      {showModal && <Modal  closeModal={closeModal} />}
     </div>
   );
 }
