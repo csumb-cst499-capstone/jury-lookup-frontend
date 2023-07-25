fc92ec80df02f779c3dea33887f6be4be049792d
AlonsoVega <alvega@csumb.edu>
2023-07-25 01:15:00 -0700

    added multilanguage feature to home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,57 @@
-"use client";
+// "use client";
 
+// import Login from "@/components/login";
+// import { PageWrapper } from "./page-wrapper";
+
+// export default function Page() {
+//   return (
+//     <PageWrapper>
+//       <div className="flex-auto">
+//         <Login />
+//       </div>
+//     </PageWrapper>
+//   );
+// }
+
+
+ "use client";
+import React from "react";
+import { I18nextProvider, useTranslation } from "react-i18next";
+import i18n from "../config/i18n";
 import Login from "@/components/login";
 import { PageWrapper } from "./page-wrapper";
 
 export default function Page() {
+  const { t } = useTranslation();
+
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  };
+
   return (
-    <PageWrapper>
-      <div className="flex-auto">
-        <Login />
-      </div>
-    </PageWrapper>
+    <I18nextProvider i18n={i18n}>
+      <PageWrapper>
+        <div className="flex-auto">
+          <Login />
+
+          <div className="language-buttons">
+            <button
+              className="language-button"
+              onClick={() => changeLanguage("en")}
+            >
+              &nbsp;
+              {t("English(US)")} 
+              &nbsp;
+            </button>
+            <button
+              className="language-button"
+              onClick={() => changeLanguage("es")}
+            >
+              {t("Español")}
+            </button>
+          </div>
+        </div>
+      </PageWrapper>
+    </I18nextProvider>
   );
 }
--- components/login.jsx
+++ components/login.jsx
@@ -1,10 +1,12 @@
 import React, { Suspense, useState } from "react";
+import { useTranslation } from "react-i18next"; // Add this import
 import { SummonDetails } from "./summon_details";
 import { Button, Input } from "@nextui-org/react";
 import Modal from "./Modal";
 import "../styles/animations.css";
 
 function Login() {
+  const { t } = useTranslation(); // Add this line to get the translation function
   const [badgeNumber, setBadgeNumber] = useState("");
   const [pinCode, setPinCode] = useState("");
   const [loggedIn, setLoggedIn] = useState(false);
@@ -56,14 +58,15 @@ function Login() {
         <div className="flex justify-center items-center h-screen">
           <div className="w-full max-w-md px-6 py-8 bg-custom-colorBox shadow-custom">
             <h3 className="text-3xl font-bold text-gray-800 text-center mb-8">
-              Jury Duty Lookup
-            </h3>
+              {t("login.juryDutyLookup")}
+              </h3>
+
             <hr className="mb-8" />
             <form>
               <div className="mb-8 shadow-customInput rounded-full">
                 <Input
                   type="text"
-                  label="Badge Number"
+                  label={t("login.badgeNumber")}
                   name="badgeNumber"
                   value={badgeNumber}
                   onChange={(e) => setBadgeNumber(e.target.value)}
@@ -75,7 +78,7 @@ function Login() {
               <div className="mb-8 shadow-customInput rounded-full">
                 <Input
                   type="password"
-                  label="Pin Code"
+                  label={t("login.pinCode")}
                   name="pinCode"
                   value={pinCode}
                   onChange={(e) => setPinCode(e.target.value)}
@@ -85,7 +88,7 @@ function Login() {
                 />
               </div>
               <div className="flex justify-center items-center mt-6">
-                <Button
+                {/* <Button
                   onClick={(event) => handleLogin(event)}
                   isDisabled={!badgeNumber || !pinCode}
                   className={`${
@@ -103,11 +106,31 @@ function Login() {
                     : buttonState === 4
                     ? "Invalid Credentials"
                     : "Sign In"}
+                </Button> */}
+                <Button
+                  onClick={(event) => handleLogin(event)}
+                  isDisabled={!badgeNumber || !pinCode}
+                  className={`${
+                    buttonState === 3
+                      ? "bg-green-500"
+                      : buttonState === 4
+                      ? "bg-red-500 animate-shake"
+                      : "bg-purple-500"
+                  } text-white font-bold px-6 py-2 rounded-lg transition-colors duration-300 ease-in-out hover:bg-opacity-75 mb-8 shadow-customSignin`}
+                >
+                  {
+                  buttonState === 2
+                    ? t("login.loading")
+                    : buttonState === 3
+                    ? t("login.success")
+                    : buttonState === 4
+                    ? t("login.invalidCredentials")
+                    : t("login.signIn")}
                 </Button>
               </div>
               <div className="text-center">
                 <Button onPress={openModal} variant="text" color="primary">
-                  Need Help?
+                  {t("login.needHelp")}
                 </Button>
                 <hr className="my-4 w-2/3 mx-auto border border-gray-300" />
               </div>
